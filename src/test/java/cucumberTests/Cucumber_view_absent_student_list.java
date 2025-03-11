979127c5746eb38939d12c4a33bf786598a40948
Adam BACHOTET-KAOUKDJI <adam.bachotet-kaoukdji@telecom-sudparis.eu>
2025-03-11 12:25:52 +0100

    Absence List

---
+++ src/test/java/cucumberTests/Cucumber_view_absent_student_list.java
@@ -0,0 +1,15 @@
+package cucumberTests;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/Features",  //folder name
+    glue ="stepDefinition" //package name for step def
+    )
+
+public class Cucumber_view_absent_student_list {
+    
+}
